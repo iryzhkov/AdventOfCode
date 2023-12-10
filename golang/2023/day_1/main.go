5e76e5d76abccdc76c8541c99718153feefcf524
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2023-12-09 18:48:02 -0800

    Added part 2 for day_1 problem

--- golang/2023/day_1/main.go
+++ golang/2023/day_1/main.go
@@ -9,7 +9,32 @@ import (
 )
 
 
-func readLines(file *os.File, lines chan string) {
+type trieNode struct {
+    value int
+    children map[rune]*trieNode
+}
+
+
+func addWord(root *trieNode, word *string, value int) {
+    var currNode, tempNode *trieNode = root, nil
+    for _, char := range(*word) {
+        tempNode = currNode.children[char]
+        if tempNode == nil {
+            tempNode = &trieNode{value: -1, children: make(map[rune]*trieNode)}
+            currNode.children[char] = tempNode
+        }
+        currNode = tempNode
+    }
+    currNode.value = value
+}
+
+
+func readLines(filename *string, lines chan string) {
+    file, err := os.Open(*filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
     var scanner *bufio.Scanner = bufio.NewScanner(file)
     for scanner.Scan() {
         lines <- scanner.Text()
@@ -22,14 +47,50 @@ func readLines(file *os.File, lines chan string) {
 }
 
 
-func extractNumber(line string) int {
+func extractNumberPart1(line *string) int {
     var first_number, second_number int = -1, 0
-    for _, char := range(line) {
+    for _, char := range(*line) {
         if '0' <= char && char <= '9' {
+            second_number = int(char - '0')
             if first_number == -1 {
                 first_number = second_number
             }
-            second_number = int(char - '0')
+        }
+    }
+    return first_number * 10 + second_number
+}
+
+func extractNumberPart2(line *string, trieRoot *trieNode) int {
+    var first_number, second_number, temp_number int = -1, 0, -1
+    var curr_nodes, temp_nodes *[]trieNode = &[]trieNode{*trieRoot}, nil
+    var next_node *trieNode
+
+    for _, char := range(*line) {
+        temp_number = -1
+        if '0' <= char && char <= '9' {
+            temp_number = int(char - '0')
+            curr_nodes = &[]trieNode{*trieRoot}
+        } else if 'a' <= char && char <= 'z' {
+            temp_nodes = &[]trieNode{*trieRoot}
+            for _, node := range(*curr_nodes) {
+                next_node = node.children[char]
+                if next_node != nil {
+                    *temp_nodes = append(*temp_nodes, *next_node)
+                    if (*next_node).value != -1 {
+                        temp_number = (*next_node).value
+                    }
+                }
+            }
+            curr_nodes = temp_nodes
+        } else {
+            curr_nodes = &[]trieNode{*trieRoot}
+        }
+
+        if temp_number != -1 {
+            second_number = temp_number
+            if first_number == -1 {
+                first_number = second_number
+            }
         }
     }
     return first_number * 10 + second_number
@@ -47,19 +108,27 @@ func main() {
             panic(errors.New("Only accept 1 or 2 arguments"))
     }
 
-    file, err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
-
     var lines_channel chan string = make(chan string, 5)
-    go readLines(file, lines_channel)
+    go readLines(&filename, lines_channel)
 
     var result int = 0
     for line := range lines_channel {
-        result += extractNumber(line)
+        result += extractNumberPart1(&line)
+    }
+    fmt.Printf("Part 1: %d\n", result)
+
+    var number_strings [10]string = [10]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+    var trieRoot *trieNode = &trieNode{value: -1, children: make(map[rune]*trieNode)}
+    for pos, number := range(number_strings) {
+        addWord(trieRoot, &number, pos)
     }
 
-    fmt.Println(result)
+    lines_channel = make(chan string, 5)
+    go readLines(&filename, lines_channel)
+
+    result = 0
+    for line := range lines_channel {
+        result += extractNumberPart2(&line, trieRoot)
+    }
+    fmt.Printf("Part 2: %d\n", result)
 }
